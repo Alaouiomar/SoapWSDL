70acb051d1ea53770f8040f33dcccf05f209508b
alaoui omar <elalaouiomar1998@gmail.com>
2022-11-18 18:14:31 +0100

    Initial commit

---
+++ ws11/src/main/java/ServerJWS.java
@@ -0,0 +1,12 @@
+import jakarta.xml.ws.Endpoint;
+
+import  ws.*;
+
+
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/", new BanqueService());
+        System.out.println("web service déployé sur http://0.0.0.0:9191");
+    }
+}
